daee2e3b5343a1521746108e6768abb8570bc563
NerellaVenkataSai <84185688+NerellaVenkataSai@users.noreply.github.com>
2023-01-15 22:56:30 +0530

    helloworld

---
+++ helloworld/main.go
@@ -0,0 +1,14 @@
+// check 21 in 01 diagrams for information of basic go program
+// 03 -- for go cli commands
+// 08 -- what is package in go ?
+// 012 -- Types of packages ?
+// 013 -- executable package (eg: package main)
+// 008 -- packages types and it's usages
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!!")
+}
